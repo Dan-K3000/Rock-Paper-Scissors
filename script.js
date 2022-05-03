3c62d57ccf88ffe88edb945591ce33613507a351
Dan K <Dan@thefrontdoor.lol>
2022-05-03 17:10:05 -0600

    Edit script.js for UI functions

--- script.js
+++ script.js
@@ -8,48 +8,64 @@ function makeTitleCase(word){
 
 function computerPlay(){
   switch(getRndInteger(1, 4)) {
-    case 1: return("Rock");
-    case 2: return("Paper");
-    case 3: return("Scissors");
+    case 1: return("rock");
+    case 2: return("paper");
+    case 3: return("scissors");
     default: return("something went horribly wrong...");
   }
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == "Rock" && computerSelection == "Scissors" ||
-    playerSelection == "Paper" && computerSelection == "Rock" ||
-    playerSelection == "Scissors" && computerSelection == "Paper")
-    return(`You Win! ${playerSelection} Beats ${computerSelection}`)
+  if (playerSelection == "rock" && computerSelection == "scissors" ||
+    playerSelection == "paper" && computerSelection == "rock" ||
+    playerSelection == "scissors" && computerSelection == "paper")
+    return(`You Win, ${playerSelection} beats ${computerSelection}`)
   else if (playerSelection == computerSelection)
     return(`Tie! You both choose ${computerSelection}`)
+
+  else if (playerSelection == "")
+    return
   else
-    return(`You Lose! ${computerSelection} Beats ${playerSelection}`)
+    return(`You Lose, ${computerSelection} beats ${playerSelection}`)
 }
 
-function game(pScore, cScore){
-  const rawAnswer = prompt("Rock, Paper, or Scissors?");
-  const player = makeTitleCase(rawAnswer);
-  const computer = computerPlay();
-  const result = playRound(player, computer)
-  const targetScore = 3;
+let pScore = 0;
+let cScore = 0;
+let win = false;
 
-  switch(result.charAt(4)){
-    case("W"):
-      pScore++;
-      break;
-    case("L"):
-      cScore++;
-      break;
+const divs = document.querySelectorAll('div');
+
+function play(e) {
+  const choice = this.id;
+  e.stopPropagation();
+  const result = playRound(choice, computerPlay());
+  const targetScore = 5;
+  const playerScore = document.querySelector(".playerScore");
+  const computerScore = document.querySelector('.computerScore');
+  const output = document.querySelector('.text');
+
+  if(win == false){
+    switch(result.charAt(4)){
+      case("W"):
+        pScore++
+        playerScore.textContent = pScore;
+        break;
+      case("L"):
+        cScore++;
+        computerScore.textContent = cScore;
+      }
+
+    if (pScore == targetScore || cScore == targetScore) {
+      if (pScore > cScore) {winner = "Player"}
+      else {winner = "Computer"}
+      output.textContent = `${result} Game Over! ${winner} wins!`
+      win = true;
     }
+    else {
+      output.textContent = result
+      console.log(result);
+    }
+  return}
+};
 
-  if (pScore == targetScore || cScore == targetScore) {
-    if (pScore > cScore) {winner = "Player"}
-    else {winner = "Computer"}
-    console.log(result)
-    console.log(`Game Over! ${winner} wins! Final score=> Computer:${cScore} | Player:${pScore}`)}
-  else {
-    console.log(`${result}  Score=> Computer:${cScore} | Player:${pScore}`)
-    game(pScore, cScore)}
-}
-
-game(0, 0);
+divs.forEach(div => div.addEventListener('click', play));
